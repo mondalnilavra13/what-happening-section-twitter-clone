9cada0fb52680aa3445c694477547cd114b174ec
mondalnilavra13 <126654958+mondalnilavra13@users.noreply.github.com>
2023-04-27 15:21:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# what-happening-section-twitter-clone
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fo5g8k)
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="shadow-box">
+        <h1 className="heading">What's happening</h1>
+        <div className="grey-text">NBA · This morning</div>
+        <div className="bold-text">Lakers at Grizzlies</div>
+        <div className="grey-text">Entertainment · Trending</div>
+        <div className="bold-text">#AishwaryaRai</div>
+        <div className="grey-text">2,806 Tweets</div>
+        <div className="grey-text">Entertainment · Trending</div>
+        <div className="bold-text">#AishwaryaRai</div>
+        <div className="grey-text">2,806 Tweets</div>
+        <div className="grey-text">Entertainment · Trending</div>
+        <div className="bold-text">#AishwaryaRai</div>
+        <div className="grey-text">2,806 Tweets</div>
+        <div className="grey-text">Entertainment · Trending</div>
+        <div className="bold-text">#AishwaryaRai</div>
+        <div className="grey-text">2,806 Tweets</div>
+        <button className="show-more-btn">Show more</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
